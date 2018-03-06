3cf747a2eb2e3fc5d72a542096ef659138486bf0
Abigail Barrera <abigailbarrera89@outlook.com>
2018-03-05 18:43:03 -0800

    updated

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express();
 var T = new Twit({
   consumer_key:         config.consumer_key,
   consumer_secret:      config.consumer_secret,
-  access_token:         config.access_key,
+  access_token:         config.access_token,
   access_token_secret:  config.access_token_secret
 });
 
